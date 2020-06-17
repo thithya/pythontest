fe5285e60a93014b320f40bfa75e0d551df5ee40
thithya <66399814+thithya@users.noreply.github.com>
2020-06-17 15:02:11 -0400

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,59 @@
+
+
+#swapping variable
+x = input(" enter the value of x:")
+y = input("Enter the valuee of y:")
+
+x , y = y , x
+print ("x=", x)
+print ("y=", y)
+
+# if statement that compares two variables and prints the larger
+
+x = float(input(" enter the value of x:"))
+y = float(input("Enter the valuee of y:"))
+
+if ( x > y):
+    print("the largest value is", x)
+else:
+    print("the largest value is", y)
+
+#larger string
+string1 = input("enter the first string:")
+string2 = input("enter the second string:")
+
+length1 = 0
+length2 = 0
+
+for i in string1:
+    length1 = length1 + 1
+
+for i in string2:
+    length2 = length2 + 1
+
+if length1 > length2 :
+    print("string1 is larger than string2")
+else:
+    print("string2 is larger than string1")
+
+#for loop to print even numbers between 1 to 100
+
+for i in range( 0, 101, 2):
+    print(i)
+
+#while loop to print even numbers
+
+i = 0
+while ( i <= 100):
+    i += 2
+    print(i)
+
+#nested loop to print astrick
+
+rows = 3
+col = 2
+for i in range(rows):
+    print('*', end='')
+    for j in range(col):
+        print('*', end= '')
+    print('')
